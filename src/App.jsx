1d927cc6af2d775c107e866b74cefceff61a9bff
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-05-05 12:09:13 +0530

    Adding Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <NavBar/>
         <Routes>
           <Route path='/' element = {<LandingPage />}></Route>
           <Route path='/about' element = {<AboutPage />}></Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   )
